ad1e0072887b722b0bed67e9b38f8bbaf10354fd
Asmit Rao <kasmit007@hotmail.com>
2025-04-24 15:40:47 +0530

    Create procedure name

---
+++ CRUD_ Stored_Procedure_Repository/Models/procedure.cs
@@ -0,0 +1,44 @@
+﻿using CRUD__Stored_Procedure_Repository.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics.Metrics;
+
+namespace CRUD__Stored_Procedure_Repository.Models
+{
+    public class procedure
+    {
+
+//        CREATE OR ALTER PROCEDURE Sp_Employees_CRUD
+//    @Action NVARCHAR(10),
+//    @Id INT = NULL,
+//    @Name NVARCHAR(100) = NULL,
+//    @Email NVARCHAR(100) = NULL
+//AS
+//BEGIN
+//        SET NOCOUNT ON;
+
+//        IF @Action = 'INSERT'
+//    BEGIN
+//        INSERT INTO Employees(Name, Email)
+//        VALUES(@Name, @Email)
+//    END
+//    ELSE IF @Action = 'UPDATE'
+//    BEGIN
+//        UPDATE Employees SET Name = @Name, Email = @Email WHERE Id = @Id
+//    END
+//    ELSE IF @Action = 'DELETE'
+//    BEGIN
+//        DELETE FROM Employees WHERE Id = @Id
+//    END
+//    ELSE IF @Action = 'SELECT'
+//    BEGIN
+//        SELECT* FROM Employees
+//    END
+//    ELSE IF @Action = 'GETBYID'
+//    BEGIN
+//        SELECT* FROM Employees WHERE Id = @Id
+//    END
+//END
+
+    }
+}
